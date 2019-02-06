cdfad69910595cd75eaf565196497f13e4a912cd
Roberts Krisjanis Plorins <cros1404@gmail.com>
2019-02-06 06:34:03 +0200

    add spiderman guy and rope

---
+++ Assets/RopeScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(HingeJoint2D))]
+[RequireComponent(typeof(HingeJoint2D))]
+
+public class RopeScript : MonoBehaviour
+{
+    private HingeJoint2D playerJoint;
+    private HingeJoint2D targetJoint;
+    private LineRenderer lineRenderer;
+
+    void Awake()
+    {
+        HingeJoint2D[] hingeJoints;
+        hingeJoints = GetComponents<HingeJoint2D>();
+        if (hingeJoints.Length != 2)
+        {
+            throw new UnityException("There should only be 2 HingeJoint2D components");
+        }
+        foreach (HingeJoint2D hingeJoint in hingeJoints)
+        {
+            if (hingeJoint.connectedBody)
+            {
+                playerJoint = hingeJoint;
+            }
+            else
+            {
+                targetJoint = hingeJoint;
+            }
+        }
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Transform currentTransform = transform;
+        Vector2 targetVector = targetJoint.anchor;
+        Vector2 playerVector = Vector2.MoveTowards(playerJoint.connectedBody.position, targetVector, 0.2f);
+        
+        Vector3[] vectors = {playerVector, targetVector};
+        lineRenderer.SetPositions(vectors);
+    }
+}
